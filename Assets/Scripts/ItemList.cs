23099dc9bb489a22a3b360cb75ee2c6bd520d291
Estevão-Matheus <matheus935@gmail.com>
2019-12-05 23:10:19 -0300

    menu de pausa

---
+++ Assets/Scripts/ItemList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ItemList : MonoBehaviour
+{
+    public Consumable_itens itemConsumivel;
+    public Text text;
+    public Image image;
+    public Weapon arma;
+    // Start is called before the first frame update
+
+    public void setUpItem (Consumable_itens item)
+    {
+        itemConsumivel = item;
+        image.sprite = itemConsumivel.imagem;
+        text.text = itemConsumivel.descrição;
+    }
+
+   public void setUpWeapon(Weapon arma1)
+    {
+        arma = arma1;
+        image.sprite = arma.image;
+        text.text = arma.descrição;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    private bool pausemenu=false;
+    public Transform cursor;
+    public GameObject pausePanel;
+    public GameObject[] menuOptions;
+    private int cursorIndex = 0;
+    public GameObject optionPanel;
+    public GameObject itemlist;
+    private Inventory iventory;
+    public GameObject itemListPrefab;
+    public RectTransform content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iventory = Inventory.inventory;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            cursorIndex = 0;
+            itemlist.SetActive(false);
+            pausemenu = !pausemenu;
+            if(pausemenu)
+            {
+                pausePanel.SetActive(true);
+            }else
+            {
+                pausePanel.SetActive(false);
+            }
+        }
+
+        if(pausemenu)
+        {
+            Vector3 cursosrPosition = menuOptions[cursorIndex].transform.position;
+            cursor.position = new Vector3(cursosrPosition.x - 100, cursosrPosition.y, cursosrPosition.z);
+            if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                cursorIndex++;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                cursorIndex--;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            optionPanel.SetActive(false);
+            itemlist.SetActive(true);
+            updateItemList(cursorIndex);
+        }
+        
+    }
+    void updateItemList(int option)
+    {
+        if(option==0)
+        {
+            for (int i = 0; i < iventory.weapons.Count;i++)
+            {
+                GameObject tempItem = Instantiate(itemListPrefab, content.transform);
+                tempItem.GetComponent<ItemList>().setUpWeapon(iventory.weapons[i]);
+            }
+        }
+
+
+    }
+
+}
